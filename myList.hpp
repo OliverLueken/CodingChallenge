f559165d4f966eeabbf9b271e2e46386575f7946
Oliver Lüken <oliver.lueken@pm.me>
2022-04-09 14:46:53 +0200

    Adding description for make_list

--- myList.hpp
+++ myList.hpp
@@ -7,6 +7,10 @@ namespace myList{
         Node* next{nullptr};
     };
 
+    /*
+    Creates a list with one element
+    Returns a pointer to a Node containing the passed in value
+    */
     Node* make_list(int value){
         auto head = new Node{value};
         return head;
