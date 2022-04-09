463e708dc2d8cc509bd9b9c906c15fb362e76f18
Oliver Lüken <oliver.lueken@pm.me>
2022-04-09 15:59:31 +0200

    Adding test for creation/deletion of a list with multiple elements

--- test_myList.cpp
+++ test_myList.cpp
@@ -31,6 +31,21 @@ TEST_CASE("List creation", "[generators]"){
         myList::delete_list(head);
         REQUIRE(head == nullptr );
     }
+
+    SECTION("Create list with a lot of elements"){
+        auto head = myList::make_list(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+        auto currentNodePtr = head;
+        int expectedValue = 1;
+        while(currentNodePtr != nullptr){
+            REQUIRE(currentNodePtr->value == expectedValue );
+            currentNodePtr = currentNodePtr->next;
+            ++expectedValue;
+        }
+
+        myList::delete_list(head);
+        REQUIRE(head == nullptr );
+    }
 }
 
 
