7af5f5c08112dcf228f9a5ad55cf8e5d2d38f05f
Oliver Lüken <oliver.lueken@pm.me>
2022-04-09 15:06:59 +0200

    Adding test for creating/destroying a list of two elements

--- test_myList.cpp
+++ test_myList.cpp
@@ -16,6 +16,21 @@ TEST_CASE("List creation", "[generators]"){
         myList::delete_list(head);
         REQUIRE(head == nullptr );
     }
+
+    SECTION("Create list with two elements"){
+        auto head = myList::make_list(1, 2);
+
+        auto currentNodePtr = head;
+        REQUIRE(currentNodePtr->value == 1 );
+        REQUIRE(currentNodePtr->next  != nullptr );
+
+        currentNodePtr = currentNodePtr->next;
+        REQUIRE(currentNodePtr->value == 2 );
+        REQUIRE(currentNodePtr->next  == nullptr );
+
+        myList::delete_list(head);
+        REQUIRE(head == nullptr );
+    }
 }
 
 
