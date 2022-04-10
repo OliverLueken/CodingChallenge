6b9ee94da5eb9b2d1d25110b312d6b3f05a6c1db
Oliver Lüken <oliver.lueken@pm.me>
2022-04-10 17:23:12 +0200

    Renaming reverseGroups to reverse_groups

--- myList.hpp
+++ myList.hpp
@@ -90,7 +90,7 @@ namespace myList{
     }
 
     template<typename ValueType>
-    Node<ValueType>* reverseGroups(Node<ValueType>*& head, const unsigned int k){
+    Node<ValueType>* reverse_groups(Node<ValueType>*& head, const unsigned int k){
         if( k<=1 ) return head;
 
         auto currentGroupHead = head;
