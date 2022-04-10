b541a0046d18e24c85a96e2c82738482a3e0fcf6
Oliver Lüken <oliver.lueken@pm.me>
2022-04-10 17:27:32 +0200

    Implementing reverse_groups

--- myList.hpp
+++ myList.hpp
@@ -57,7 +57,7 @@ namespace myList{
     /*
     Receives a pointer to a list nodePtr and a positive integer n and
     returns the pointer to the nth next element in the list
-    returns nullptr if list contains less than n+1 elements 
+    returns nullptr if list contains less than n+1 elements
     */
     template<typename ValueType>
     Node<ValueType>* advance(Node<ValueType>* nodePtr, unsigned int n){
@@ -97,11 +97,39 @@ namespace myList{
         auto currentGroupTail = advance(head, k-1);
         const auto newHead = currentGroupTail;
 
+        //remove group from list
+        auto nextGroupHead = currentGroupTail->next;
+        currentGroupTail->next = nullptr;
+
+        //reverse group
+        auto currentGroupHeadReversed = reverse_list(currentGroupHead);
+        auto currentGroupTailReversed = currentGroupHead;
+
+        //insert group
+        currentGroupTailReversed->next = nextGroupHead;
+
+        //update pointer
+        currentGroupTail = advance(nextGroupHead, k-1);
+
         while(currentGroupTail != nullptr){
+            auto lastGroupTail = currentGroupTailReversed;
+            currentGroupHead = nextGroupHead;
+
             //remove group from list
+            lastGroupTail->next = nullptr;
+            nextGroupHead = currentGroupTail->next;
+            currentGroupTail->next = nullptr;
+
             //reverse group
+            currentGroupHeadReversed = reverse_list(currentGroupHead);
+            currentGroupTailReversed = currentGroupHead;
+
             //insert group
+            lastGroupTail->next = currentGroupHeadReversed;
+            currentGroupTailReversed->next = nextGroupHead;
+
             //update pointer
+            currentGroupTail = advance(nextGroupHead, k-1);
         }
 
         return newHead;
