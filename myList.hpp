4c6ae9c76d974a8ef01b6798a0417595d4f89fca
Oliver Lüken <oliver.lueken@pm.me>
2022-04-27 17:30:24 +0200

    Updating make_list to return List

--- myList.hpp
+++ myList.hpp
@@ -36,9 +36,11 @@ namespace myList{
     A list created with make_list has to be destroyed with delete_list to not leak memory
     */
     template<typename ValueType>
-    Node<ValueType>* make_list(ValueType&& value){
-        auto head = new Node<ValueType>{std::forward<ValueType>(value)};
-        return head;
+    List<ValueType> make_list(ValueType&& value){
+        return List<ValueType>{
+            new Node<ValueType>{std::forward<ValueType>(value)},
+            &delete_list<ValueType>
+        };
     }
 
     /*
@@ -46,116 +48,117 @@ namespace myList{
     Returns a pointer to the head of the list
     */
     template<typename ValueType, typename... ValueTypes>
-    Node<ValueType>* make_list(ValueType&& value, ValueTypes&&... values){
-        auto head = new Node<ValueType>{std::forward<ValueType>(value)};
-        head->next = make_list(std::forward<ValueTypes>(values)...);
+    List<ValueType> make_list(ValueType&& value, ValueTypes&&... values){
+        auto head  = make_list(std::forward<ValueType>(value));
+        head->next = make_list(std::forward<ValueTypes>(values)...).release();
         return head;
     }
 
-    template<typename ValueType>
-    void print(const Node<ValueType>* head){
-        auto currentNodePtr = head;
-        while(currentNodePtr != nullptr){
-            std::cout << currentNodePtr->value << ' ';
-            currentNodePtr = currentNodePtr->next;
-        }
-        std::cout << '\n';
-    }
-
-    /*
-    Receives a pointer to a list nodePtr and a positive integer n and
-    returns the pointer to the nth next element in the list
-    returns nullptr if list contains less than n+1 elements
-    */
-    template<typename ValueType>
-    Node<ValueType>* advance(Node<ValueType>* nodePtr, unsigned int n){
-        while(n>0 && nodePtr != nullptr){
-            nodePtr = nodePtr->next;
-            --n;
-        }
-        return nodePtr;
-    }
-
-    /*
-    Reverses the elements inside the list
-    Returns a pointer to the head of the reversed list
-    The passed in head pointer stays valid and points to the last element in the list
-    */
-    template<typename ValueType>
-    Node<ValueType>* reverse_list(Node<ValueType>* head){
-        auto lastNodePtr = head;
-        auto currentNodePtr = head->next;
-
-        head->next = nullptr;
-        while(currentNodePtr != nullptr){
-            auto nextNodePtr = currentNodePtr->next;
-            currentNodePtr->next = lastNodePtr;
-
-            lastNodePtr = currentNodePtr;
-            currentNodePtr = nextNodePtr;
-        }
-        return lastNodePtr;
-    }
-
-    /*
-    Removes the nodes following nodePtr from the list
-    Return pointer to the node that followed nodePtr
-    */
-    template<typename ValueType>
-    Node<ValueType>* split_after(Node<ValueType>* nodePtr){
-        auto newListHead = nodePtr->next;
-        nodePtr->next = nullptr;
-        return newListHead;
-    }
-
-    /*
-    Merges two lists
-    */
-    template<typename ValueType>
-    void merge_lists(Node<ValueType>* firstList, Node<ValueType>* secondList){
-        auto lastNodePtr = firstList;
-        while(lastNodePtr->next != nullptr){
-            lastNodePtr = lastNodePtr->next;
-        }
-        lastNodePtr->next = secondList;
-    }
-
-
-    /*
-    Devides list in groups of size k and reverses each group. Nodes at the end of the list that do not fill a whole group are not reversed
-    Returns a pointer to the head of the modified list
-    */
-    template<typename ValueType>
-    Node<ValueType>* reverse_groups(Node<ValueType>* head, const unsigned int k){
-        if( k<=1 ) return head;
-
-        auto reverseNodes = [](auto& ptrToGroupHead, auto ptrToGroupTail){
-            //remove group from list
-            auto nextGroupHead = split_after(ptrToGroupTail);
-
-            //reverse group
-            ptrToGroupTail = ptrToGroupHead;
-            ptrToGroupHead = reverse_list(ptrToGroupHead);
-
-            //insert group
-            merge_lists(ptrToGroupTail, nextGroupHead);
-
-            return ptrToGroupTail;
-        };
-
-        auto currentGroupTail = advance(head, k-1);
-        if(currentGroupTail == nullptr) return head; //k is greater than nodes in the list
-
-        auto previousGroupTail = reverseNodes(head, currentGroupTail);
-        currentGroupTail = advance(previousGroupTail, k);
-
-        while(currentGroupTail != nullptr){
-            previousGroupTail = reverseNodes(previousGroupTail->next, currentGroupTail);
-            currentGroupTail = advance(previousGroupTail, k);
-        }
-
-        return head;
-    }
+    //
+    // template<typename ValueType>
+    // void print(const Node<ValueType>* head){
+    //     auto currentNodePtr = head;
+    //     while(currentNodePtr != nullptr){
+    //         std::cout << currentNodePtr->value << ' ';
+    //         currentNodePtr = currentNodePtr->next;
+    //     }
+    //     std::cout << '\n';
+    // }
+    //
+    // /*
+    // Receives a pointer to a list nodePtr and a positive integer n and
+    // returns the pointer to the nth next element in the list
+    // returns nullptr if list contains less than n+1 elements
+    // */
+    // template<typename ValueType>
+    // Node<ValueType>* advance(Node<ValueType>* nodePtr, unsigned int n){
+    //     while(n>0 && nodePtr != nullptr){
+    //         nodePtr = nodePtr->next;
+    //         --n;
+    //     }
+    //     return nodePtr;
+    // }
+    //
+    // /*
+    // Reverses the elements inside the list
+    // Returns a pointer to the head of the reversed list
+    // The passed in head pointer stays valid and points to the last element in the list
+    // */
+    // template<typename ValueType>
+    // Node<ValueType>* reverse_list(Node<ValueType>* head){
+    //     auto lastNodePtr = head;
+    //     auto currentNodePtr = head->next;
+    //
+    //     head->next = nullptr;
+    //     while(currentNodePtr != nullptr){
+    //         auto nextNodePtr = currentNodePtr->next;
+    //         currentNodePtr->next = lastNodePtr;
+    //
+    //         lastNodePtr = currentNodePtr;
+    //         currentNodePtr = nextNodePtr;
+    //     }
+    //     return lastNodePtr;
+    // }
+    //
+    // /*
+    // Removes the nodes following nodePtr from the list
+    // Return pointer to the node that followed nodePtr
+    // */
+    // template<typename ValueType>
+    // Node<ValueType>* split_after(Node<ValueType>* nodePtr){
+    //     auto newListHead = nodePtr->next;
+    //     nodePtr->next = nullptr;
+    //     return newListHead;
+    // }
+    //
+    // /*
+    // Merges two lists
+    // */
+    // template<typename ValueType>
+    // void merge_lists(Node<ValueType>* firstList, Node<ValueType>* secondList){
+    //     auto lastNodePtr = firstList;
+    //     while(lastNodePtr->next != nullptr){
+    //         lastNodePtr = lastNodePtr->next;
+    //     }
+    //     lastNodePtr->next = secondList;
+    // }
+    //
+    //
+    // /*
+    // Devides list in groups of size k and reverses each group. Nodes at the end of the list that do not fill a whole group are not reversed
+    // Returns a pointer to the head of the modified list
+    // */
+    // template<typename ValueType>
+    // Node<ValueType>* reverse_groups(Node<ValueType>* head, const unsigned int k){
+    //     if( k<=1 ) return head;
+    //
+    //     auto reverseNodes = [](auto& ptrToGroupHead, auto ptrToGroupTail){
+    //         //remove group from list
+    //         auto nextGroupHead = split_after(ptrToGroupTail);
+    //
+    //         //reverse group
+    //         ptrToGroupTail = ptrToGroupHead;
+    //         ptrToGroupHead = reverse_list(ptrToGroupHead);
+    //
+    //         //insert group
+    //         merge_lists(ptrToGroupTail, nextGroupHead);
+    //
+    //         return ptrToGroupTail;
+    //     };
+    //
+    //     auto currentGroupTail = advance(head, k-1);
+    //     if(currentGroupTail == nullptr) return head; //k is greater than nodes in the list
+    //
+    //     auto previousGroupTail = reverseNodes(head, currentGroupTail);
+    //     currentGroupTail = advance(previousGroupTail, k);
+    //
+    //     while(currentGroupTail != nullptr){
+    //         previousGroupTail = reverseNodes(previousGroupTail->next, currentGroupTail);
+    //         currentGroupTail = advance(previousGroupTail, k);
+    //     }
+    //
+    //     return head;
+    // }
 }
 
 #endif
